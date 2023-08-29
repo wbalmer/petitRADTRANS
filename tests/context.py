59f37bc0f352920ed6ededfbc2aa0f7817494df4
Doriann Blain <doriann.blain@gmail.com>
2023-08-29 17:03:14 +0200

    Test installed package rather than development files

--- tests/context.py
+++ tests/context.py
@@ -1,8 +1,9 @@
 import os
 import sys
 
-# Ensure that we are testing the package development files
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# Ensure that we are testing the package installed (*not* the development) files
+del sys.path[0]  # remove current directory from searching path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # add the package directory at the end
 
 import petitRADTRANS
 
