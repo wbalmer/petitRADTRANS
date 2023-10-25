d92dddee88d6d53f90bb2026cd77df29d9a31749
Doriann Blain <doriann.blain@gmail.com>
2023-10-25 12:50:42 +0200

    Add new plot functions in plotlib init all

--- petitRADTRANS/plotlib/__init__.py
+++ petitRADTRANS/plotlib/__init__.py
@@ -1,6 +1,11 @@
 from .plotlib import (
     contour_corner,
+    plot_cloud_condensation_curves,
+    plot_data,
+    plot_multiple_posteriors,
+    plot_posterior,
     plot_radtrans_opacities,
+    plot_result_corner
 )
 from .style import (
     get_species_color,
@@ -15,7 +20,12 @@ __all__ = [
     'contour_corner',
     'flux_units',
     'get_species_color',
+    'plot_cloud_condensation_curves',
+    'plot_data',
+    'plot_multiple_posteriors',
+    'plot_posterior',
     'plot_radtrans_opacities',
+    'plot_result_corner',
     'set_petitradtrans_plot_style',
     'update_figure_font_size',
     'wavelength_units',
