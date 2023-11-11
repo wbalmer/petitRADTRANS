3611970ed7d14204127a260707d398d34e989b6e
Doriann Blain <doriann.blain@gmail.com>
2023-11-11 12:42:29 +0100

    Fix test

--- tests/test_radtrans_line_by_line.py
+++ tests/test_radtrans_line_by_line.py
@@ -59,10 +59,10 @@ def init_spectral_model_line_by_line():
         # Instrument parameters
         new_resolving_power=radtrans_parameters['mock_observation_parameters']['high_resolution_resolving_power'],
         output_wavelengths=np.arange(
-            radtrans_parameters['mock_observation_parameters']['wavelength_range_high_resolution'][0],# * 1e-4,
-            radtrans_parameters['mock_observation_parameters']['wavelength_range_high_resolution'][1],# * 1e-4,
+            radtrans_parameters['mock_observation_parameters']['wavelength_range_high_resolution'][0],
+            radtrans_parameters['mock_observation_parameters']['wavelength_range_high_resolution'][1],
             radtrans_parameters['mock_observation_parameters']['wavelength_range_high_resolution'][0] /
-            radtrans_parameters['mock_observation_parameters']['high_resolution_resolving_power'] / 2# * 1e-4
+            radtrans_parameters['mock_observation_parameters']['high_resolution_resolving_power'] / 2
         ),  # um
         wavelength_boundaries=radtrans_parameters['spectrum_parameters']['wavelength_range_line_by_line'],
         # Scaling parameters
