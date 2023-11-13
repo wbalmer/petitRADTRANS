be40f54a25b30f9b8f4bc82786cd15db66ba5067
mauricemolli <der.paule@gmail.com>
2023-11-13 16:23:37 +0100

    Increased version number.

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = u'2019-2023, Paul Molli\u00E8re'
 author = u'Paul Molli\u00E8re'
 
 # The full version, including alpha/beta/rc tags
-release = '2.7.3'
+release = '2.7.4'
 
 
 # -- General configuration ---------------------------------------------------
