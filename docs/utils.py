8cbb73f92a89a20a40e749e0381bef2a7b683a37
Doriann Blain <doriann.blain@gmail.com>
2024-03-15 15:41:26 +0100

    Fix c-k species table not displaying charge and displaying long short names

--- docs/utils.py
+++ docs/utils.py
@@ -5,9 +5,80 @@ import os
 
 from petitRADTRANS._input_data_loader import (
     _get_base_cloud_names, _split_species_source,
-    get_input_data_subpaths, get_species_basename, join_species_all_info, split_species_all_info
+    get_input_data_subpaths, get_species_basename, get_species_isotopologue_name,
+    join_species_all_info, split_species_all_info
 )
 from petitRADTRANS.config import petitradtrans_config_parser
+
+
+def make_table_ck_species_reference_documentation(path_to_ck_opacities=None):
+    """Display the list of c-k opacity references in rST table format.
+    Intended to be used in docs/content/available_opacities.rst
+
+    Args:
+        path_to_ck_opacities: path to folder containing the c-k opacities.
+    """
+    if path_to_ck_opacities is None:
+        path_to_ck_opacities = os.path.join(
+            petitradtrans_config_parser.get_input_data_path(),
+            get_input_data_subpaths()['correlated_k_opacities']
+        )
+
+    species_list = sorted(glob.glob(path_to_ck_opacities + '/*/*/*'))
+    return_string = ""
+
+    for species in species_list:
+        file_name = species.rsplit(os.path.sep, 1)[-1]
+
+        name, natural_abundance, charge, _, source, _ = split_species_all_info(file_name)
+
+        call_name = join_species_all_info(
+            name=name,
+            natural_abundance=natural_abundance,
+            charge=charge,
+            source=source
+        )
+
+        base_name = get_species_basename(name)
+        main_isotopologue_name = get_species_isotopologue_name(base_name)
+        short_charge = charge.split('_', 1)[-1]
+
+        if name == main_isotopologue_name:
+            call_name_short = base_name + short_charge
+        else:
+            call_name_short = join_species_all_info(
+                name=name,
+                natural_abundance=natural_abundance,
+                charge=short_charge
+            )
+
+        with h5py.File(species, 'r') as f:
+            doi = f['DOI'][0].decode('utf-8')
+
+            contributor = '--'
+
+            if 'contributor' in f['DOI'].attrs:
+                _contributor = f['DOI'].attrs['contributor']
+
+                if _contributor is not None:
+                    if 'karan' in _contributor:
+                        contributor = '`K. Molaverdikhani <mailto:karan.molaverdikhani@colorado.edu>`_'
+                    elif 'exomol' in _contributor:
+                        contributor = (
+                            '`Chubb et al. (2021) <https://ui.adsabs.harvard.edu/abs/2021A%26A...646A..21C/abstract>`_,'
+                            ' `ExoMolOP <https://www.exomol.com/data/data-types/opacity/>`_'
+                        )
+
+        return_string += (
+            f"\n"
+            f"    * - {call_name_short}\n"
+            f"      - {call_name}\n"
+            f"      - {file_name}\n"
+            f"      - {doi}\n"
+            f"      - {contributor}"
+        )
+
+    print(return_string)
 
 
 def make_table_cloud_species_reference_documentation(path_to_cloud_opacities=None):
@@ -70,63 +141,6 @@ def make_table_cloud_species_reference_documentation(path_to_cloud_opacities=Non
             f"    * - {call_name}\n"
             f"      - {file_name}\n"
             f"      - {doi}"
-        )
-
-    print(return_string)
-
-
-def make_table_ck_species_reference_documentation(path_to_ck_opacities=None):
-    """Display the list of c-k opacity references in rST table format.
-    Intended to be used in docs/content/available_opacities.rst
-
-    Args:
-        path_to_ck_opacities: path to folder containing the c-k opacities.
-    """
-    if path_to_ck_opacities is None:
-        path_to_ck_opacities = os.path.join(
-            petitradtrans_config_parser.get_input_data_path(),
-            get_input_data_subpaths()['correlated_k_opacities']
-        )
-
-    species_list = sorted(glob.glob(path_to_ck_opacities + '/*/*/*'))
-    return_string = ""
-
-    for species in species_list:
-        file_name = species.rsplit(os.path.sep, 1)[-1]
-
-        name, _, charge, cloud_info, source, _ = split_species_all_info(file_name)
-        cloud_info = cloud_info.rsplit('_', 1)[0]  # discard space group or amorphous name info
-
-        call_name = join_species_all_info(
-            name=name,
-            cloud_info=cloud_info,
-            source=source
-        )
-
-        with h5py.File(species, 'r') as f:
-            doi = f['DOI'][0].decode('utf-8')
-
-            contributor = '--'
-            try:
-                cID = f['DOI'].attrs['contributor']
-                if cID is not None:
-                    if 'karan' in cID:
-                        contributor = '`K. Molaverdikhani <mailto:karan.molaverdikhani@colorado.edu>`_'
-                    elif 'exomol' in cID:
-                        contributor = '`Chubb et al. (2021) <https://ui.adsabs.harvard.edu/abs/2021A%26A...646A..21C/abstract>`_, `ExoMolOP <https://www.exomol.com/data/data-types/opacity/>`_'
-            except KeyError:
-                pass
-
-        call_name_short = call_name.split('__')[0]
-
-        # TODO add charge information here
-        return_string += (
-            f"\n"
-            f"    * - {call_name_short}\n"
-            f"      - {call_name}\n"
-            f"      - {file_name}\n"
-            f"      - {doi}\n"
-            f"      - {contributor}"
         )
 
     print(return_string)
