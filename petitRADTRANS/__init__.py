cdb8c2ca13061fb1ff9f870104f0a73630870151
Doriann Blain <doriann.blain@gmail.com>
2024-03-22 17:10:16 +0100

    Fix Windows crash when loading HDF5 files in some cases

--- petitRADTRANS/__init__.py
+++ petitRADTRANS/__init__.py
@@ -4,8 +4,15 @@ import sys
 
 __version__ = importlib.metadata.version("petitRADTRANS")
 
-# Link to the libs folders on Windows
+# Windows fixes
 if sys.platform == 'win32':
+    # Overrides default HDF5 behaviour to prevent crashes when loading HDF5 files
+    if 'HDF5_USE_FILE_LOCKING' not in os.environ:
+        print("No HDF5 file locking handling explicitly setup by an environment variable, "
+              "setting 'HDF5_USE_FILE_LOCKING' to 'FALSE'...")
+        os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
+
+    # Link to the libs folders on Windows
     if sys.version_info >= (3, 8):
         '''
         The fortran extensions need libgfortran*.dll to work but for some reason it does not (always?) work even if the
