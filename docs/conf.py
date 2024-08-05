34782f4ffd942d69e3c8061dc36f7b02c811b4bf
mauricemollie <der.paule@gmail.com>
2024-08-05 22:27:36 +0200

    Merge with remote.

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ copyright = u'2019-2024, the pRT team'
 author = u'pRT team'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.0a30'
+release = '3.1.0a31'
 
 
 # -- General configuration ---------------------------------------------------
