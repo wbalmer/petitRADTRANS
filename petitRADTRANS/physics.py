Polishing non-uniform resolution, fixed bug in effective temperature calculation (need to specify units)

petitRADTRANS/physics.py
@@ -36,7 +36,7 @@ def compute_dist(t_irr, dist, t_star, r_star, mode, mode_what):
         return dist
 
 
-def compute_effective_temperature(wavelengths, flux, orbit_semi_major_axis=1.0, planet_radius=1.0):
+def compute_effective_temperature(wavelengths, flux, orbit_semi_major_axis=1.0, planet_radius=1.0, use_si_units=False):
     """Calculates the effective temperature by integrating the model and using the stefan boltzmann law.
 
     Args:
@@ -48,15 +48,19 @@ def compute_effective_temperature(wavelengths, flux, orbit_semi_major_axis=1.0, 
             Distance to the object. Must have same units as planet_radius
         planet_radius : Optional(float)
             Object radius. Must have same units as orbit_semi_major_axis
+        use_si_units : Optional(bool)
+            If the flux is in W/m2/micron, this should be true
     """
     def integrate_flux(_wavelengths, _flux):
         return np.sum(
             _flux[:-1] * ((orbit_semi_major_axis / planet_radius) ** 2.) * np.diff(_wavelengths)
         )
+    unit_factor = 1.0
+    if use_si_units: unit_factor=1e-3
 
     energy = integrate_flux(wavelengths, flux)
 
-    return (energy / cst.sigma) ** 0.25
+    return (energy / (cst.sigma*unit_factor)) ** 0.25
 
 
 def doppler_shift(wavelength_0, velocity):

petitRADTRANS/retrieval/data.py
@@ -368,7 +368,7 @@ class Data:
 
     def intialise_data_resolution(self):
         if isinstance(self.data_resolution,np.ndarray):
-            self.data_resolution_array_model = np.interp(self.radtrans_object,self.wavelengths,self.data_resolution)
+            self.data_resolution_array_model = np.interp(1e4*cst.c/self.radtrans_object._frequencies,self.wavelengths,self.data_resolution)
 
     def update_bins(self, wlens):
         self.wavelength_bin_widths = np.zeros_like(wlens)
@@ -457,7 +457,11 @@ class Data:
                     model_spectra.append(spectrum_model)
 
                 for spectrum_model in model_spectra:
-                    if self.data_resolution is not None:
+                    if self.data_resolution_array_model is not None:
+                        spectrum_model = self.convolve(wlen_model,
+                                                       spectrum_model,
+                                                       self.data_resolution_array_model)
+                    elif self.data_resolution is not None:
                         spectrum_model = self.convolve(wlen_model,
                                                        spectrum_model,
                                                        self.data_resolution)

petitRADTRANS/retrieval/retrieval.py
@@ -2381,7 +2381,7 @@ class Retrieval:
                 else:
                     wlen, model, __ = ret_val
 
-                tfit = compute_effective_temperature(wlen, model, params["D_pl"].value, params["planet_radius"].value)
+                tfit = compute_effective_temperature(wlen, model, params["D_pl"].value, params["planet_radius"].value,use_si_units=True)
                 teffs.append(tfit)
             tdict[name] = np.array(teffs)
             np.save(f"{self.output_directory}evaluate_{name}/{name}_sampled_teff", np.array(teffs))