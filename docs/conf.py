9c4d289aa42329c40cfaf8dd10b22da07a90f98b
Doriann Blain <doriann.blain@gmail.com>
2024-10-04 09:17:16 +0200

    Fix version in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ copyright = u'2019-2024, the pRT team'
 author = u'pRT team'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.0'
+release = '3.1.1'
 
 
 # -- General configuration ---------------------------------------------------
